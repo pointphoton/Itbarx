e90729797cf2fff9208e97cdc0b1c13fa50ff8f8
pointphoton <pointphotonone@gmail.com>
2015-09-09 05:09:10 +0300

    bark sayfası yapılıyor

---
+++ app/src/main/java/com/itbarx/activity/BarkActivity.java
@@ -0,0 +1,23 @@
+package com.itbarx.activity;
+
+import android.content.Context;
+
+/**
+ * TODO: Add a class header comment!
+ */
+public class BarkActivity extends BaseActivity {
+    @Override
+    protected int getLayoutResourceId() {
+        return 0;
+    }
+
+    @Override
+    protected Context getContext() {
+        return BarkActivity.this;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+}
